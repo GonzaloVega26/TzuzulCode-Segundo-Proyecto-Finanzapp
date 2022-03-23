3b4a3c84ce6ab5a841fe73f47babc10ac641876e
Gonzalo Vega <31gonzalovega@gmail.com>
2022-03-23 10:07:02 -0300

    Added new queries to Dao for Payment

--- app/src/main/java/com/gonzalo/vega/tzuzulcodesegundoproyectofinanzapp/database/CardDao.kt
+++ app/src/main/java/com/gonzalo/vega/tzuzulcodesegundoproyectofinanzapp/database/CardDao.kt
@@ -2,24 +2,50 @@ package com.gonzalo.vega.tzuzulcodesegundoproyectofinanzapp.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import com.gonzalo.vega.tzuzulcodesegundoproyectofinanzapp.models.Payment
 import com.gonzalo.vega.tzuzulcodesegundoproyectofinanzapp.models.Card
+import com.gonzalo.vega.tzuzulcodesegundoproyectofinanzapp.models.relations.CardWithPayments
 
 @Dao
 interface CardDao {
     //If the fun returns a liveData element it doesn't need suspend
     //Suspend: the code can be paused or resumed
     @Insert
-    suspend fun insert(card:Card)
+    suspend fun insertCard(card:Card)
 
     @Delete
-   suspend fun delete(card:Card)
+   suspend fun deleteCard(card:Card)
 
     @Update
-    suspend fun update(card:Card)
+    suspend fun updateCard(card:Card)
+
 
     @Query("SELECT * FROM cards")
-    fun readAll():LiveData<List<Card>>
+    fun readAllCards():LiveData<List<Card>>
 
     @Query("SELECT * FROM cards WHERE idCard = :idCard")
-    fun readOne(idCard:Long):LiveData<Card>
+    fun readOneCard(idCard:Long):LiveData<Card>
+
+        /* DAO FOR TRANSACTIONS*/
+    @Insert
+    suspend fun insertTransaction(payment : Payment)
+
+    @Delete
+    suspend fun deleteTransaction(payment : Payment)
+
+    @Update
+    suspend fun updateTransaction(payment : Payment)
+
+
+    @Query("SELECT * FROM transactions")
+    fun readAllTransactions():LiveData<List<Payment>>
+
+    @Query("SELECT * FROM transactions WHERE idPayment= :idPayment")
+    fun readOneTransaction(idPayment:Long):LiveData<Payment>
+
+    @Transaction
+    @Query("SELECT * FROM cards WHERE idCard = :idCard")
+    fun getOneCardWithPayments(idCard: Long): List<CardWithPayments>
+
+
 }
